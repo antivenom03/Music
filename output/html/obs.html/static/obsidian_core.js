0265fd3b8d0d646abfdebd00ec23575c45e00b63
Jo <jojoivy03@gmail.com>
2023-02-06 02:39:54 +0000

    Deploy

--- output/html/obs.html/static/obsidian_core.js
+++ output/html/obs.html/static/obsidian_core.js
@@ -10,7 +10,7 @@ var CONFIGURED_HTML_URL_PREFIX = "/Music";
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '61252030518102080574853987813818645794'                       // used to check whether the localStorage data is stale
+var gzip_hash = '316293817243771689505888734982996799967'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
